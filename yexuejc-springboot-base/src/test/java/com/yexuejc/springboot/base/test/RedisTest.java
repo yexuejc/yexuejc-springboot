31cb9c5b55cc7e3097781b057da2530f9adf24bb
yexuejc <940526mf>
2018-09-28 15:17:01 +0800

    新增声明包

--- yexuejc-springboot-base/src/test/java/com/yexuejc/springboot/base/test/RedisTest.java
+++ yexuejc-springboot-base/src/test/java/com/yexuejc/springboot/base/test/RedisTest.java
@@ -2,6 +2,7 @@ package com.yexuejc.springboot.base.test;
 
 import com.yexuejc.base.util.JsonUtil;
 import com.yexuejc.springboot.base.ApplicationRun;
+import com.yexuejc.springboot.base.autoconfigure.MutiRedisAutoConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,30 +26,33 @@ import java.util.concurrent.TimeUnit;
 @SpringBootTest(classes = ApplicationRun.class)
 public class RedisTest {
     @Autowired
-    @Qualifier(RedisAutoConfiguration.BEAN_REDIS_TEMPLATE0)
+    @Qualifier(MutiRedisAutoConfiguration.BEAN_REDIS_TEMPLATE0)
     RedisTemplate redisTemplate;
 
     @Test
     public void insert() {
-        Map<String,Object> map = new HashMap<>();
-        map.put("a","b");
-        map.put("c",1);
-        map.put("aac",true);
-        map.put("q","asdb");
-        redisTemplate.opsForHash().putAll("test.redis",map);
-        redisTemplate.expire("test.redis",10, TimeUnit.MINUTES);
+        Map<String, Object> map = new HashMap<>();
+        map.put("a", "b");
+        map.put("c", 1);
+        map.put("aac", true);
+        map.put("q", "asdb");
+        redisTemplate.opsForHash().putAll("test.redis", map);
+        redisTemplate.expire("test.redis", 10, TimeUnit.MINUTES);
     }
+
     @Test
     public void get() {
-        Map<String,Object> map = redisTemplate.opsForHash().entries("test.redis");
+        Map<String, Object> map = redisTemplate.opsForHash().entries("test.redis");
         System.out.println(JsonUtil.obj2Json(map));
     }
+
     @Test
     public void update() {
-        Map<String,Object> map = redisTemplate.opsForHash().entries("test.redis");
-        redisTemplate.opsForHash().put("test.redis","q","123456798");
+        Map<String, Object> map = redisTemplate.opsForHash().entries("test.redis");
+        redisTemplate.opsForHash().put("test.redis", "q", "123456798");
         System.out.println(JsonUtil.obj2Json(map));
     }
+
     @Test
     public void del() {
         Boolean delete = redisTemplate.delete("test.redis");
